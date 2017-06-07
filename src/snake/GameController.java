2219702cf28c0351425b7e32540ccf01c029ddeb
Ashley <Ashley@ASUSSAPPHIRE>
2017-06-07 18:53:39 +0100

    Added comments

--- src/snake/GameController.java
+++ src/snake/GameController.java
@@ -10,7 +10,9 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 
 /**
- *
+ * Controls the entire functioning of the game including drawing graphical elements,
+ * calculating game states, controlling movement and actions and processing
+ * keyboard input.
  * @author Ashley
  */
 public class GameController {
@@ -28,6 +30,14 @@ public class GameController {
     private enum Direction {NORTH, EAST, SOUTH, WEST}
     private Direction direction;
     
+    /**
+     * Initialises a new GameController with the specified parameters and
+     * immediately creates a new game state.
+     * @param gc the GraphicsContext that the game will be drawn to
+     * @param gridWidth the width of the game grid
+     * @param gridHeight the height of the game grid
+     * @param cellSize the size in pixels of each cell in the grid
+     */
     public GameController(GraphicsContext gc, int gridWidth, int gridHeight, int cellSize) {
         this.gc = gc;
         this.gridWidth = gridWidth;
@@ -37,6 +47,9 @@ public class GameController {
         startGame();
     }
     
+    /**
+     * Initialise the game
+     */
     private void startGame() {
         createSnake();
         createApple();
@@ -44,6 +57,9 @@ public class GameController {
         score = 0;
     }
     
+    /**
+     * Create a new snake at the centre of the game grid
+     */
     private void createSnake() {
         int halfWidth = gridWidth / 2;
         int halfHeight = gridHeight / 2;
@@ -53,20 +69,37 @@ public class GameController {
         }
     }
     
+    /**
+     * Kill the snake :(
+     */
     private void killSnake() {
         snake.clear();
     }
     
+    /**
+     * Add a cell to the snake at the specified coordinates
+     * @param x the x coordinate of the grid location to add
+     * @param y the y coordinate of the grid location to add
+     */
     private void addSnakeCell(int x, int y) {
         snake.add(new Point2D(x, y));
     }
     
+    /**
+     * Create the apple at a random location on the game grid.
+     * TODO: Fix so it doesn't generate the apple on top of the snake
+     */
     private void createApple() {
         Random rnd = new Random();
         apple = new Point2D(rnd.nextInt(gridWidth), rnd.nextInt(gridHeight));
     }
     
+    /**
+     * Draws the game to the GraphicsContext
+     */
     private void drawGame() {
+        //For  each each cell of the grid if there is a snake draw green,
+        //an apple draw red or nothing draw white.
         for (int x = 0; x < gridWidth; x++) {
             for (int y = 0; y < gridHeight; y++) {
                 if (snake.contains(new Point2D(x, y))) {
@@ -78,22 +111,35 @@ public class GameController {
                 }
             }
         }
+        //Draw a grid around the game grid
         gc.strokeRect(0, 0, gridWidth * cellSize, gridHeight * cellSize);        
     }
     
+    /**
+     * Draws a cell of the specified colour and x and y coordinates to the game grid
+     * @param x the x coordinate of the cell to draw
+     * @param y the y coordinate of the cell to draw
+     * @param color the Color of the cell to draw
+     */
     private void drawCell(int x, int y, Color color) {
         gc.setFill(color);
         gc.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
     }
     
+    /**
+     * Move the snake in the current direction
+     */
     private void moveSnake() {
         Point2D snakeTail = snake.get(snake.size() - 1);
         
+        //Starting from the end of the snake set each cell to the position of the
+        //next cell except for the head
         for (int i = snake.size() - 1; i > 0; i--) {
             snake.set(i, new Point2D(snake.get(i - 1).getX(), snake.get(i - 1).getY()));
         }
-//        System.out.println("snake is " + snake.size() + " cells big");
         
+        //For the head of the snake calculate its new position based on the
+        //current direction
         Point2D snakeHeadCoords = snake.get(0);
         switch (direction) {
             case NORTH:
@@ -111,25 +157,43 @@ public class GameController {
         }
         snake.set(0, snakeHeadCoords);
         
+        //If the snake has eaten an apple append the cell that previously was
+        //the tail to the end of the snake
         if (shouldGrow) {
             addSnakeCell((int) snakeTail.getX(), (int) snakeTail.getY());
             shouldGrow = false;
         }        
     }
     
+    /**
+     * Calculates whether the snake has eaten an apple
+     * @return true if the snake's head is in the same position as the apple
+     */
     private boolean hasEatenApple() {
         return apple.equals(snake.get(0));
     }
     
+    /**
+     * Calculates whether the snake has collided
+     * @return true if the snake has collided with itself or the edge of the
+     * game grid
+     */
     private boolean hasCollided() {
+        //Has the snake collided with itself?
         for (int i = 1; i < snake.size(); i++) {
             if (snake.get(0).equals(snake.get(i))) {
                 return true;
             }
         }
+        //Has the snake colided with the edge of the game grid?
         return snake.get(0).getX() < 0 || snake.get(0).getX() > gridWidth || snake.get(0).getY() < 0 || snake.get(0).getY() > gridHeight;
     }
     
+    /**
+     * Calculates the next state of the game. First moves the snake then checks
+     * for collisions with itself, the edge of the game grid and the apple. Then
+     * draws the updated game state.
+     */
     public void tick() {
         if (!paused) {
             moveSnake();
@@ -147,6 +211,10 @@ public class GameController {
         keyPressedThisTick = false;        
     }
     
+    /**
+     * Handles keyboard input
+     * @param ke the KeyEvent to handle
+     */
     public void handleKeyPressed(KeyEvent ke) {
         KeyCode keyCode = ke.getCode();
         if (!keyPressedThisTick && !paused) {
@@ -172,10 +240,18 @@ public class GameController {
         ke.consume();
     }
     
+    /**
+     * Returns the value of score
+     * @return score
+     */
     public int getScore() {
         return score;
     }
     
+    /**
+     * Returns whether the game is paused or not
+     * @return paused
+     */
     public boolean isPaused() {
         return paused;
     }
--- src/snake/Snake.java
+++ src/snake/Snake.java
@@ -1,6 +1,5 @@
 package snake;
 
-import java.awt.GraphicsEnvironment;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.geometry.Insets;
@@ -15,7 +14,9 @@ import javafx.stage.Stage;
 
 
 /**
- *
+ * The main class that handles displaying the game window and receiving user
+ * input. Also handles some graphics elements such as labels as well as
+ * providing the AnimationTimer for the game to function.
  * @author Ashley
  */
 public class Snake extends Application {
@@ -27,31 +28,42 @@ public class Snake extends Application {
         final int cellSize = 20;
         final int frameDelay = 50_000_000;
         
+        //The canvas that will display the game
         final Canvas canvas = new Canvas(gridWidth * cellSize, gridHeight * cellSize);
         
+        //A label to display the player's current score
         final Label scoreLbl = new Label("Score: 0");
         StackPane.setAlignment(scoreLbl, Pos.TOP_LEFT);
         StackPane.setMargin(scoreLbl, new Insets(10));
         scoreLbl.setFont(new Font("Courier New", 20));
         
+        //A label to display whether the game is paused or not
         final Label pausedLbl = new Label();
         StackPane.setAlignment(pausedLbl, Pos.TOP_RIGHT);
         StackPane.setMargin(pausedLbl, new Insets(10));
         pausedLbl.setFont(new Font("Courier New", 20));
         
+        //A StackPane to group the nodes together
         final StackPane root = new StackPane(canvas, scoreLbl, pausedLbl);
         
+        //The main scene
         final Scene scene = new Scene(root);
         
+        //A new GameController to handle the workings of the game
         final GameController game = new GameController(canvas.getGraphicsContext2D(), gridWidth, gridHeight, cellSize);
         
+        //Set it so any keyboard input is redirected to the GameController
         scene.setOnKeyPressed((KeyEvent t) -> game.handleKeyPressed(t));
         
+        //An animation timer which limits the framerate of the game and handles
+        //updating of the score and paused labels.
         new AnimationTimer() {
             long updateNanoTime = System.nanoTime();
             
             @Override
             public void handle(long currentNanoTime) {
+                //If enough time has passed between ticks then calculate the
+                //next game state and update the labels
                 if (currentNanoTime - updateNanoTime >= frameDelay) {
                     game.tick();
                     scoreLbl.setText("Score: " + game.getScore());
